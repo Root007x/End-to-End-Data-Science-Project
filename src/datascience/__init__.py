45e5d332612e55b09fb486bd4a35706faa5be3cc
unknown <mahadihasanpm@gmail.com>
2025-05-20 06:57:52 +0600

    Logging Functionality

--- src/datascience/__init__.py
+++ src/datascience/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 import logging
 
-logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]" # log format
 
 log_dir = "logs"
 log_file_path = os.path.join(log_dir,"logging.log")
@@ -10,13 +10,13 @@ os.makedirs(log_dir, exist_ok=True)
 
 
 logging.basicConfig(
-    level = logging.INFO,
+    level = logging.INFO, # Sets the minimum log level to INFO, so it will log INFO, WARNING, ERROR, CRITICAL messages
     format = logging_str,
 
 
     handlers = [
-        logging.FileHandler(log_file_path),
-        logging.StreamHandler(sys.stdout)
+        logging.FileHandler(log_file_path), # writes all log messages
+        logging.StreamHandler(sys.stdout) # prints all log messages to the console
     ]
 )
 
