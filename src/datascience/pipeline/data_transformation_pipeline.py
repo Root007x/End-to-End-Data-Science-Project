7530f717afe853c012d0a7be88f4a71b80750588
unknown <mahadihasanpm@gmail.com>
2025-05-21 01:04:08 +0600

    data transformation

--- src/datascience/pipeline/data_transformation_pipeline.py
+++ src/datascience/pipeline/data_transformation_pipeline.py
@@ -1,6 +1,7 @@
 from src.datascience.config.configuration import ConfigurationManager
 from src.datascience.components.data_transformation import DataTransformation
 from src.datascience import logger
+from pathlib import Path
 
 
 
@@ -12,10 +13,19 @@ class DataTransformationTrainingPipeline:
         pass
 
     def initiate_data_transformation(self):
-        config = ConfigurationManager()
-        data_transformation_config = config.get_data_transformation_config()
-        data_transformation = DataTransformation(config=data_transformation_config)
-        data_transformation.train_test_splitting()
+
+        try:
+            with open(Path("artifacts/data_validation/status.txt"), "r") as f:
+                status  = f.read().split(" ")[-1]
+            if status == "True":
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_splitting()
+            else:
+                raise Exception("Your Data Schema is not valid")
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
